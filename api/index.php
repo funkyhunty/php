e528a581e2facb17ceda768b99fe4c46cec8c5bb
362227 <91358231+362227@users.noreply.github.com>
2023-10-12 21:27:32 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,36 @@
+<meta name="referrer" content="never">
 <?php
-phpinfo();
+// 获取并验证用户输入
+$path = isset($_GET['path']) ? $_GET['path'] : '';
+$path = urlencode($path);
+
+// 验证输入，确保只包含合法字符
+if (preg_match('/^[a-zA-Z0-9_\-\.\/\%]*$/', $path)) {
+    // 合法的输入，继续处理
+    $path = urldecode($path);
+    $initialUrl = "https://onemanager-php--forevervideo.repl.co/$path";
+    $userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36";
+    $referer = "https://www.aliyundrive.com"; // 替换成你想要的 Referer 地址
+
+    $curlCmd = 'curl -A "' . $userAgent . '" -e "' . $referer . '" -I -L "' . $initialUrl . '"';
+    $headers = shell_exec($curlCmd);
+
+    if (strpos($headers, "Location:") !== false) {
+        // 从HTTP标头中提取跳转后的URL
+        preg_match('/Location: (.+)/', $headers, $matches);
+        $redirectUrl = trim($matches[1]);
+      //  echo '<a href="' . $redirectUrl . '"> Click</a>';
+       // exit;
+
+        // 在浏览器中打开跳转后的URL
+        header("Location: " . $redirectUrl);
+        exit;
+    } else {
+        // 未发生跳转，你可以根据需要进行处理
+        echo "Error" ;
+    }
+} else {
+    // 非法输入，返回错误或采取其他安全措施
+    echo "Invalid input";
+}
+?>
