748393a49de3c693344938c2afa8e3f3624c8303
362227 <91358231+362227@users.noreply.github.com>
2023-11-18 11:00:54 +0800

    Create img.php

---
+++ api/img.php
@@ -0,0 +1,32 @@
+<?php
+function url_origin( $s, $use_forwarded_host = false )
+{
+    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
+    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+    $port     = $s['SERVER_PORT'];
+    $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
+    $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
+    $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+    return $protocol . '://' . $host;
+}
+
+function full_url( $s, $use_forwarded_host = false )
+{
+    return url_origin( $s, $use_forwarded_host ) . $s['REQUEST_URI'];
+}
+
+$absolute_url = full_url( $_SERVER );
+$url = preg_replace('/.+?\?img\=(.*)/','$1', $absolute_url);
+
+
+$img = file_get_contents($url);
+
+if ($img !== false) {
+
+    header("Content-type: image/jpeg");
+    echo $img;
+} else {
+    echo "无法获取图片内容。";
+}
+?>
